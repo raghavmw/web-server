3d4f02bcbd2c6278b8a5ea8c92ffec2ec4240a41
Raghav <raghavendrakumar.anandan@sjsu.edu>
2018-02-02 22:06:10 -0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {                                          // app.use() takes only 1 function as argument
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to log into file');
+    }
+  })
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));                                 // Express Middleware
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  //res.send('<h1>Hello Express</h1>');
+  // res.send({
+  //   name: 'Raghav',
+  //   likes: [
+  //     'Soccer',
+  //     'Driving'
+  //   ]
+  // });
+
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    bodyMsg: 'Welcome to my home page',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to load the page'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server started at port 3000');
+});
